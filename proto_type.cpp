0f3e741ad6a465bf5d82514b0404ef6f9ba4e764
JubaerHossain <jubaer01.cse@gmail.com>
2022-09-10 20:50:00 +0600

    vector with iterator

--- proto_type.cpp
+++ proto_type.cpp
@@ -2,25 +2,28 @@
 #include <algorithm>
 #include <vector>
 #include <cmath>
+#include<iterator>
 using namespace std; 
-int sum(int a, int b);
 
 int main()
 {
-    int a, b;
+    vector<int> v;
+    vector<int>::iterator it;
+    v.push_back(10);
+    v.push_back(20);
+    v.push_back(30);
+    v.push_back(40);
+    v.push_back(50);
+    v.push_back(60);
 
-    cout << "Enter two numbers: ";
-    cin >> a ;
-    cout << "Enter two numbers: ";
-    cin >> b ;
 
-    cout << "Sum is : " << sum(a, b) << endl;
-
-
+    for(it = v.begin(); it != v.end(); it++)
+    {
+        cout << *it <<endl;
+    }
+    cout << "v.size() = " << v.size() <<endl;
+    cout << "v.capacity() = " << v.capacity() <<endl;
+    cout << "v.max_size() = " << v.max_size() <<endl;
+    cout << "v.front() = " << v.front() <<endl;
     return 0;
-}
-
-int sum(int a, int b)
-{
-    return a + b;
 }
