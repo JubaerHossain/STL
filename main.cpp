08dbb07741041da5f04e10bfac053bc3c27168b2
JubaerHossain <jubaer01.cse@gmail.com>
2022-09-10 21:03:06 +0600

    reverse

--- main.cpp
+++ main.cpp
@@ -1,14 +1,22 @@
-#include<iostream>
-using namespace std;
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include<iterator>
+using namespace std; 
 
 int main()
 {
-    int a;
-    int *p;
-    a=10;
-    p = &a;
-    cout<< *p <<endl;
-    *p = 20;
-    cout <<"a = "<<a<<endl;
+    vector<int> v = {60, 20, 40, 30, 50};
+    vector<int>::iterator it;
+    // sorting the vector
+    sort(v.begin(), v.end());
+    // descending order
+    reverse(v.begin(), v.end());
+    // printing the vector
+    for(it = v.begin(); it != v.end(); it++)
+    {
+        cout << *it << " ";
+    }
     return 0;
 }
