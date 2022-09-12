123b47959dceb42239eaaace6cbb189f0b450de8
JubaerHossain <jubaer01.cse@gmail.com>
2022-09-12 07:53:27 +0600

    insert & search

--- main.cpp
+++ main.cpp
@@ -10,16 +10,53 @@ int main()
     
     map<string, int> m;
 
+
     m["a"] = 21; // map do not allow duplicate keys
     m["c"] = 33; // map do ascending order by default with respect to keys
     m["b"] = 2;
     m["d"] = 47;
     m["e"] = 50;
-
-    for (auto it = m.begin(); it != m.end(); it++)
+    map<string, int>::iterator it;
+    for (it = m.begin(); it != m.end(); it++)
     {
         cout << it->first << " " << it->second << endl;
     }
+    cout << endl;
+
+    // insertion in map
+    pair<string, int> p;
+    p.first = "f";
+    p.second = 100;
+    m.insert(p);
+
+    // another way of insertion in map
+
+    m.insert(make_pair("g", 200));
+
+
+    // another way of insertion in map
+
+    m["h"] = 300;
+
+    for (it = m.begin(); it != m.end(); it++)
+    {
+        cout << it->first << " " << it->second << endl;
+    }
+    cout << endl;
+    
+
+
+    // search in map
+    string key = "h";
+    if (m.count(key) == 0)
+    {
+        cout << key << " is not present" << endl;
+    }
+    else
+    {
+        cout << key << " is present" << endl;
+    }
+
     
     return 0;
 }
