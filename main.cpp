ab1858d264dd64b0a6b426dd78a901cd1f69bf37
JubaerHossain <jubaer01.cse@gmail.com>
2022-09-16 10:03:46 +0600

    ques

--- main.cpp
+++ main.cpp
@@ -10,10 +10,29 @@ int main()
     s.push("Hello");
     s.push("World");
     s.push("!");
+
+    // front
     while (!s.empty())
     {
         cout << s.front() << endl;
         s.pop();
+    }
+
+    cout << endl;
+
+    // back
+
+    queue<int> s2;
+
+    s2.push(1);
+    s2.push(2);
+    s2.push(3);
+
+
+    while (!s2.empty())
+    {
+        cout << s2.back() << endl;
+        s2.pop();
     }
 
     return 0;
