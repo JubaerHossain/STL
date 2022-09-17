064d8ffab763ada44810916b9a83be9611bdfe64
JubaerHossain <jubaer01.cse@gmail.com>
2022-09-17 23:46:10 +0600

    sorting

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    vector<int> v = {1, 22, 3, 24, 5, 6, 7, 8, 9, 10};
 
     // binary search
     bool bsearch = binary_search(v.begin(), v.end(), 58);
@@ -26,6 +26,16 @@ int main()
     auto it2 = upper_bound(v.begin(), v.end(), 5);
 
     cout << "Upper Bound of 5 is at position " << it2 - v.begin() << endl;
+
+
+    //SORT
+
+    sort(v.begin(), v.end());
+
+    for (int x : v)
+        cout << x << " ";
+
+    cout << endl;
 
 
     
