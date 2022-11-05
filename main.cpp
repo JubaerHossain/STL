4f7b4741454da96655a35eac302b96bb8bb366ae
JubaerHossain <jubaer01.cse@gmail.com>
2022-11-05 22:53:12 +0600

    unordered map

--- main.cpp
+++ main.cpp
@@ -9,6 +9,8 @@ int main()
     m.insert({1, 3});
     m.insert({1, 4});
     m.insert({1, 5});
+    m.insert({1, 5});
+    m.emplace(1, 6);
     m.emplace(1, 6);
     cout << "Size of map is " << m.size() << endl;
     map<string, int>::iterator it;
@@ -38,14 +40,14 @@ int main()
     }
 
     // erase the element
-    cout << "Erase the element" << endl;
+    cout << "Erase the element of first" << endl;
     m.erase(m.begin());
     for (auto it = m.begin(); it != m.end(); it++)
     {
         cout << it->first << " " << it->second << endl;
     }
     // erase the all index of element
-    cout << "erase the all index of element" << endl;
+    cout << "erase the 2 index of element" << endl;
     m.erase(2);
     for (auto it = m.begin(); it != m.end(); it++)
     {
