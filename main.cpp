5787c309de3beb55e8f41ecae112a15afb3209de
JubaerHossain <jubaer01.cse@gmail.com>
2022-11-05 23:15:39 +0600

    dequeue

--- main.cpp
+++ main.cpp
@@ -6,15 +6,52 @@ using namespace std;
 
 int main()
 {
-    stack<string> s;
-    s.push("Hello");
-    s.push("World");
-    s.push("!");
-    while(!s.empty())
-    {
-        cout << s.top() << endl;
-        s.pop();
-    }
+    deque<string> dq;
+    dq.push_back("a");
+    dq.push_back("b");
+    dq.push_back("c");
+
+    for (auto it = dq.begin(); it != dq.end(); ++it)
+        cout << *it << endl;
+
+
+    // pop
+    dq.pop_front();
+    cout << "after front pop" << endl;
+    
+    for (auto it = dq.begin(); it != dq.end(); ++it)
+        cout << *it << endl;
+    dq.pop_back();
+
+    cout << "after back pop" << endl;    
+    for (auto it = dq.begin(); it != dq.end(); ++it)
+        cout << *it << endl;
+
+
+    // empty
+
+    if (dq.empty())
+        cout << "dq is empty" << endl;
+    else
+        cout << "dq is not empty" << endl;
+
+    // size
+
+    cout << "dq size is " << dq.size() << endl;
+
+    // clear
+
+    dq.clear();
+
+    if (dq.empty())
+        cout << "dq is empty" << endl;
+    else
+        cout << "dq is not empty" << endl;
+
+
+
+    
+
     
     return 0;
 }
