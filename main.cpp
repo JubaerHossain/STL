089c558359522a3b703236a12c7381d32c7ef98b
JubaerHossain <jubaer01.cse@gmail.com>
2022-11-06 01:46:11 +0600

    list

--- main.cpp
+++ main.cpp
@@ -10,52 +10,62 @@ int main()
 {
     list<int> l;
     l.push_back(1);
-
     l.push_back(200);
     l.push_back(30);
-    l.push_front(4);
     l.push_back(4);
+    // push for begin
+    l.push_front(5);
 
     list<int>::iterator it = l.begin();
 
     for (it = l.begin(); it != l.end(); it++)
         cout << *it << " ";
-    l.sort();
-    l.reverse();
-
     cout << endl;
+    cout << "after sort" << endl;
+    l.sort();
 
     for (it = l.begin(); it != l.end(); it++)
         cout << *it << " ";
     cout << endl;
+    cout << "after reverse" << endl;
+    l.reverse();
+
+    for (it = l.begin(); it != l.end(); it++)
+        cout << *it << " ";
+
+    cout << endl;
+
+
     it++;
     it++;
+    cout << "after insert 100" << endl;
     l.insert(it, 100);
 
-    for ( it = l.begin(); it != l.end(); it++ )
+    for (it = l.begin(); it != l.end(); it++)
         cout << *it << " ";
     cout << endl;
     // find the target number
-    it = find(l.begin(), l.end(), 300);
-    if (it == l.end()){
+    cout << "find 100" << endl;
+    it = find(l.begin(), l.end(), 100);
+    if (it == l.end())
+    {
         cout << "not found" << endl;
     }
-    else{
+    else
+    {
         cout << "found at :" << *it << endl;
     }
-
 
     cout << endl;
 
     // delete the target number
-   
-    it = find(l.begin(), l.end(), 30);
+    cout << "delete 100" << endl;
+
+    it = find(l.begin(), l.end(), 100);
     l.erase(it);
 
-    for ( it = l.begin(); it != l.end(); it++ )
+    for (it = l.begin(); it != l.end(); it++)
         cout << *it << " ";
-
-    
 
     return 0;
 }
