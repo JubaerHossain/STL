cc907e2db3a2ce3b8780b21e8e163324c1894c5b
JubaerHossain <jubaer01.cse@gmail.com>
2022-11-05 15:35:47 +0600

    details

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     unordered_set<int> s;
-    
+
     s.insert(1);
     s.insert(2);
     s.insert(3);
@@ -18,9 +18,9 @@ int main()
     cout << "After erasing 2" << endl;
     for (auto it = s.begin(); it != s.end(); it++)
         cout << *it << " ";
-        cout << endl;
+    cout << endl;
 
-     s.erase(s.begin());
+    s.erase(s.begin());
 
     cout << "After erasing first element" << endl;
 
@@ -28,6 +28,14 @@ int main()
         cout << *it << " ";
     cout << endl;
 
+    if (s.empty() == false)
+        cout << "The unordered set is not empty " << endl;
+    else
+        cout << "The unordered set is empty" << endl;
+
+    s.clear();
+    
+    cout << "Size of the unordered set after clearing all the elements: " << s.size();
 
     return 0;
 }
